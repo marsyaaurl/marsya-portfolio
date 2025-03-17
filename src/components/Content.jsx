38fb17a0594b1508f1aec8a6480e966d24448e71
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-03-17 23:38:28 +0700

    responsivity

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -16,67 +16,58 @@ function Content() {
 
   return (
     <>
-      <div className="mt-5 ml-5 flex flex-row mr-5">
-        <img src={img1} className="w-1/2 p-12" />
-        <div className="p-12 flex flex-col">
-          <p className="text-secondgreen font-extrabold text-2xl mb-0">
+      <div className="mt-5 mx-5 flex flex-col items-center md:flex-row md:items-center">
+        <img src={img1} className="w-3/4 xs:w-full md:w-1/2 p-6 md:p-12 mx-auto" />
+        <div className="p-6 md:p-12 flex flex-col text-center xs:text-center md:text-left">
+          <p className="text-secondgreen font-extrabold text-lg md:text-2xl mb-1">
             Jakarta, Indonesia
           </p>
-          <p className="text-mainblue font-black text-7xl mb-3">
+          <p className="text-mainblue font-black text-3xl xs:text-4xl md:text-6xl mb-3">
             marsya aurelia
           </p>
-          <p
-            className="text-xl font-extrabold mb-3 px-3 py-2 rounded-full w-fit transition-all duration-700 ease-in-out bg-thirdyellow text-thirdred"
-          >
-            <Typewriter
-              words={roles}
-              loop={0}
-              cursor
-              cursorStyle="_"
-              typeSpeed={80}
-              deleteSpeed={50}
-              delaySpeed={1000}         
-            />
+          <p className="text-lg xs:text-xl md:text-xl font-extrabold mb-3 px-3 py-2 rounded-full w-fit mx-auto md:mx-0 bg-thirdyellow text-thirdred">
+            <Typewriter words={roles} loop={0} cursor cursorStyle="_" typeSpeed={80} deleteSpeed={50} delaySpeed={1000} />
           </p>
-
-          <p className="font-extralight">
+          <p className="font-extralight text-sm xs:text-base md:text-base">
             um hi, i’m marsya and i’m an undergraduate compsci student majoring
             in information systems. i mainly work with react.js and always seek
             for opportunities to learn something new.
           </p>
-          <div className="mt-3 flex flex-col gap-2">
-            <p className="text-mainblue font-extrabold text-xl">Languages</p>
-            <div className="flex flex-row gap-5 bg-thirdpink w-fit rounded-full px-6 py-2">
-                <i class="fa-brands fa-js text-4xl text-secondgreen"></i>
-                <i class="fa-brands fa-html5 text-4xl text-secondgreen"></i>
-                <i class="fa-brands fa-css3-alt text-4xl text-secondgreen"></i>
-                <i class="fa-solid fa-c text-4xl text-secondgreen"></i>
-                <i class="fa-brands fa-java text-4xl text-secondgreen"></i>
+          <div className="mt-3">
+            <p className="text-mainblue font-extrabold text-lg xs:text-xl md:text-xl md:text-left">Languages</p>
+            <div className="flex flex-wrap items-center justify-center md:justify-start gap-3 bg-thirdpink w-fit rounded-full px-4 py-2 mx-auto md:mx-0">
+              <i className="fa-brands fa-js text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
+              <i className="fa-brands fa-html5 text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
+              <i className="fa-brands fa-css3-alt text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
+              <i className="fa-solid fa-c text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
+              <i className="fa-brands fa-java text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
             </div>
           </div>
-          <div className="mt-3 flex flex-col gap-2">
-            <p className="text-mainblue font-extrabold text-xl">Frameworks</p>
-            <div className="flex flex-row gap-5 bg-thirdpink w-fit rounded-full px-6 py-2">
-                <i class="fa-brands fa-react text-4xl text-secondgreen"></i>
-                <i class="fa-brands fa-laravel text-4xl text-secondgreen"></i>
-                <i class="fa-brands fa-node text-4xl text-secondgreen"></i>
+
+          <div className="mt-3">
+            <p className="text-mainblue font-extrabold text-lg xs:text-xl md:text-xl md:text-left">Frameworks</p>
+            <div className="flex flex-wrap items-center justify-center md:justify-start gap-3 bg-thirdpink w-fit rounded-full px-4 py-2 mx-auto md:mx-0">
+              <i className="fa-brands fa-react text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
+              <i className="fa-brands fa-laravel text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
+              <i className="fa-brands fa-node text-xl xs:text-2xl md:text-3xl text-secondgreen"></i>
             </div>
           </div>
         </div>
       </div>
-      <div className="mt-32 ml-5 mr-5 p-12 overflow-hidden">
-        <p className="text-black text-5xl font-black">projects</p>
-        <div className="flex flex-row gap-5 overflow-x-auto mt-20 no-scrollbar ml-32">
-          <img src={StepIn} className="h-64 object-contain" />
-          <img src={Andro} className="h-64 object-contain" />
-          <img src={webPorto} className="h-64 object-contain w-auto" />
-          <img src={HIMA} className="h-64 object-contain" />
+      <div className="mt-20 mx-5 p-6 md:p-12 overflow-hidden">
+        <p className="text-black text-2xl xs:text-3xl md:text-4xl font-black">projects</p>
+        <div className="flex flex-row gap-4 overflow-x-auto mt-8 no-scrollbar pl-8 pr-0">
+          <img src={StepIn} className="h-32 xs:h-40 md:h-56 object-contain" />
+          <img src={Andro} className="h-32 xs:h-40 md:h-56 object-contain" />
+          <img src={webPorto} className="h-32 xs:h-40 md:h-56 object-contain" />
+          <img src={HIMA} className="h-32 xs:h-40 md:h-56 object-contain" />
         </div>
-        <div className="flex flex-row gap-5 overflow-x-auto mt-10 no-scrollbar">
-          <img src={pawshop} className="h-64 object-contain" />
-          <img src={Trax} className="h-64 object-contain" />
-          <img src={PKKMB} className="h-64 object-contain w-auto" />
-          <img src={Merch} className="h-64 object-contain" />
+
+        <div className="flex flex-row gap-4 overflow-x-auto mt-8 no-scrollbar pl-8 pr-0">
+          <img src={pawshop} className="h-32 xs:h-40 md:h-56 object-contain" />
+          <img src={Trax} className="h-32 xs:h-40 md:h-56 object-contain" />
+          <img src={PKKMB} className="h-32 xs:h-40 md:h-56 object-contain" />
+          <img src={Merch} className="h-32 xs:h-40 md:h-56 object-contain" />
         </div>
       </div>
     </>
