5486bcf510da3754d241792b776cba512d6c994f
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-06-22 10:11:43 +0700

    font size contacts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,23 +13,23 @@ function About() {
                     <div className="flex flex-row gap-x-3">
                         <a href="https://github.com/marsyaaurl" target="_blank">
                             <div className="flex flex-row gap-x-2 items-center rounded-full px-3 py-1 bg-whitefont hover:bg-gray-400">
-                                <i className="fa-brands fa-github text-background text-3xl bg-transparent"></i>
-                                <p className="text-background text-lg font-semibold bg-transparent">Github</p>
+                                <i className="fa-brands fa-github text-background text-2xl bg-transparent"></i>
+                                <p className="text-background text-md font-semibold bg-transparent">Github</p>
                             </div>
                         </a>
                         <a href="www.linkedin.com/in/marsyaaurl" target="_blank">
                             <div className="flex flex-row gap-x-2 items-center rounded-full px-3 py-1 bg-whitefont hover:bg-gray-400">
-                                <i className="fa-brands fa-linkedin text-background text-3xl bg-transparent"></i>
-                                <p className="text-background text-lg font-semibold bg-transparent">Linkedin</p>
+                                <i className="fa-brands fa-linkedin text-background text-2xl bg-transparent"></i>
+                                <p className="text-background text-md font-semibold bg-transparent">Linkedin</p>
                             </div>
                         </a>
                         <a href="https://instagram.com/marsyaaurl" target="_blank">
                             <div className="flex flex-row gap-x-2 items-center rounded-full px-3 py-1 bg-whitefont hover:bg-gray-400">
-                                <i className="fa-brands fa-instagram text-background text-3xl bg-transparent"></i>
-                                <p className="text-background text-lg font-semibold bg-transparent">Instagram</p>
+                                <i className="fa-brands fa-instagram text-background text-2xl bg-transparent"></i>
+                                <p className="text-background text-md font-semibold bg-transparent">Instagram</p>
                             </div>
                         </a>
-                        <button className="bg-mainblue text-whitefont font-bold hover:bg-gray-400 rounded-full px-5 py-2 text-lg w-fit">Download CV</button>
+                        <button className="bg-mainblue text-whitefont font-bold hover:bg-gray-400 rounded-full px-5 py-2 text-md w-fit">Download CV</button>
                     </div>
                 </div>
                 <div className="flex flex-col px-5 md:px-10 md:py-20 gap-y-3 md:w-1/2">
